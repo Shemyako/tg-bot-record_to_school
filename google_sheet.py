12f966aa9faa56586fe1201498ccb3f54acb2360
Shemyako <52855609+Shemyako@users.noreply.github.com>
2022-08-05 13:08:34 +0300

    Google module created
    Google module created;
    Telebot file created

---
+++ google_sheet.py
@@ -0,0 +1,17 @@
+import gspread
+
+class googleSheet():
+    # Конструктор. Получает имя файла, в котором токены и ссылку на google sheet
+    def __init__(self, Filename:str, Tablename:str, ):
+        self.__filename = Filename
+        self.__tablename = Tablename
+        self.__update
+        self.__gc = gspread.service_account(self.filename)
+        
+    # Обновление данных их google sheet по ссылке
+    def __update(self):
+        self.__sh = gc.open_by_url(self.tablename)
+        self.__values = print(sh.sheet1.get_all_values())
+
+
+
